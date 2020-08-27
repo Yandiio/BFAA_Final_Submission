1589e3e94e07c753d9b5e44b5acb05fa33ae8209
Yandiio <yandifenanda95@gmail.com>
2020-08-28 04:32:27 +0700

    create new splash screen

---
+++ Submission3/app/src/main/java/com/dicoding/github/lastsubmission/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.github.lastsubmission.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.github.lastsubmission.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
