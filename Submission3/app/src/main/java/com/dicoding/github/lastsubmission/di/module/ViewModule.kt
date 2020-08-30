7fd9d45180f3ac3c3f129d78e1c8b09c3bb44f45
Yandiio <yandifenanda95@gmail.com>
2020-08-30 08:45:19 +0700

    created viewmodelfactory and modifying module

--- Submission3/app/src/main/java/com/dicoding/github/lastsubmission/di/module/ViewModule.kt
+++ Submission3/app/src/main/java/com/dicoding/github/lastsubmission/di/module/ViewModule.kt
@@ -1,11 +1,12 @@
 package com.dicoding.github.lastsubmission.di.module
 
+import com.dicoding.github.lastsubmission.viewmodel.ViewModelFactory
 import dagger.Binds
 import dagger.Module
 
 @Module
-abstract class ViewModule {
+abstract class ViewModelModule {
 
-//    @Binds
-//    internal abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory)
+    @Binds
+    internal abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory) : ViewModelFactory
 }
